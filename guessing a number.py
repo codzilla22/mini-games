10ed35957178b828e511bb6442cbcfa04ac180b6
Hammad Tariq <113939608+codzilla22@users.noreply.github.com>
2022-10-03 22:33:04 +0500

    Add files via upload

---
+++ guessing a number.py
@@ -0,0 +1,41 @@
+import random
+
+print("Welcome to Guess a number!")
+choice=input ("Do you want to play? ")
+print(choice)
+if choice.lower()=="yes":
+    print("Let's start:) ")
+
+top= input("What is your range of numbers: ")
+top=int(top)
+
+if top<=0:
+    print("Please enter a number larger than zero. Thank you")
+    top= input("What is your range of numbers: ")
+    top=int(top)
+
+else:
+    random_number=random.randint(0,top)
+
+
+guesses=0
+
+while True:
+    guesses=guesses+1
+    user_guess=input("Guess a number: ")
+    if user_guess.isdigit():
+        user_guess= int(user_guess)
+    else:
+        print("Please enter a number again: ")
+        continue
+
+    if user_guess==random_number:
+        print("You got it")
+        break
+    if user_guess>random_number:
+        print("You were above the number")
+    else:
+        print("You were below the number")
+        
+
+print("You got it in " + str(guesses) + " guesses")
