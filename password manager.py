265fab53cf562b5c8a55097e377b2e3139b4b380
Hammad Tariq <113939608+codzilla22@users.noreply.github.com>
2022-10-09 01:23:39 -0700

    Add files via upload
    Since it is a beginner project it only lets you hardcode your master password and you can only access the rest of the data if you know the key(master password).
    The code lets you create, write and view your passwords in a text file and secure the text file with a grand key.

---
+++ password manager.py
@@ -0,0 +1,49 @@
+
+def view():
+    with open("passwords.txt", "r") as f:
+        for line in f.readlines():
+            data=(line.rstrip())
+            user, passw, acc = data.split("|")
+            print ("User: " + user )
+            print ("Password: " + passw)
+            print ("Account: " + acc)
+
+            
+def add():
+    user_name=input("Account name: ")
+    pwd=input("Password: ")
+    account= input("Account for: ")
+    with open("passwords.txt", "a") as f:
+        f.write(user_name + "|" + pwd + "|" + account + "\n")
+
+
+i=0
+
+while i<3:
+        
+    passwords=["codz324430.hamad"]
+    master_pwd=input("Input a login password: ")
+
+    if master_pwd not in passwords[0]:
+        print ("Invalid input.Try again: ")
+        i=i+1
+
+        if i==3:
+            print("You are a pirate. ")
+            quit()
+    else:
+        print("Welcome")
+        while True:
+
+            choice=input("Would you like to view an existing password or add a new password(Enter view or add)? You can press q to quit: ")
+
+            if choice=="q":
+                quit()
+
+            if choice=="view":
+                view()
+            elif choice=="add":
+                add()
+            else:
+                print ("Invalid input. ")
+                continue
